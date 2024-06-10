5bf5a54d5a556f133cacd857bc1d4f2c82771613
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-06-10 13:54:50 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,304 @@
+// 1chi masala
+// function getLastChar(str) {
+//     let a = 'salom'
+//     let res = a.at(-1);
+//     console.log(res);
+// }
+// getLastChar()
+
+
+// 3chi masala
+// function getFirstCharCode(str) {
+//     let a = 'salom'
+//     res = a.charCodeAt(0);
+//     console.log(res);
+// }
+// getFirstCharCode();
+
+
+// 4chi masala
+// function getStringLengthDescription(str) {
+//     let a = "assalomu alaykum"
+//     let res;
+//     if (a.length > 10) {
+//         console.log(a.length);
+//         res = 'uzoq string'
+//     } else {
+//         console.log(a.length);
+//         res = 'uzoq string'
+//     }
+//     console.log(res);
+// }
+// getStringLengthDescription();
+
+
+// 5chi masala
+// function getCharCodes(str) {
+//     let a = 'salom'
+//     let res = a.charCodeAt(0);
+//     let res1 = a.charCodeAt(1)
+//     let res2 = a.charCodeAt(2)
+//     let res3 = a.charCodeAt(3)
+//     let res4 = a.charCodeAt(4)
+//     console.log(res);
+//     console.log(res1);
+//     console.log(res2);
+//     console.log(res3);
+//     console.log(res4);
+// }
+// getCharCodes()
+
+
+//  6chi masala
+// function getFirstThreeChars(str){
+// let a = 'salom'
+// let res = a.slice(0,3)
+// console.log(res);
+// }
+// getFirstThreeChars();
+
+
+// 7chi masala
+// function getLastFiveChars(str) {
+//     let a = 'assalomu alekum'
+//     let res = a.slice(9, -1);
+//     console.log(res);
+//     return res
+// }
+// getLastFiveChars()
+
+
+// 8chi masala
+// function getMiddleChars(str) {
+//     let res;
+//     if (str.length % 2 == 1) {
+//         let middleIndex = Math.trunc(str.length / 2);
+//         res = str.substring(middleIndex = middleIndex + 1)
+//     }
+//     return res
+// }
+// console.log(getMiddleChars("hello"));
+
+
+// 9chi masala
+// function getThreeCharsFromFifthIndex(str) {
+//     let res = str.substr(5, 3)
+//     return res
+// }
+// console.log(getThreeCharsFromFifthIndex('assalomu alaykumm'));
+
+
+// 10chi masala
+// function getFirstTenChars(str) {
+//     let res = str.substring(0, 10);
+//     return res
+// }
+// console.log(getFirstTenChars('assalomu alaykum'));
+
+
+// 11chi masala
+// function getLastThreeChars(str) {
+//     let res;
+//     if (str.length % 2 == 1) {
+//         let middleIndex = Math.trunc(str.length / 2);
+//         res = str.substring(middleIndex = middleIndex)
+//     }
+//     return res
+// }
+// console.log(getMiddleChars("hello"));
+
+
+// 12chi masala
+// function getSubstringFromSecondIndex(str) {
+//     let res = str.substring(2);
+//     return res;
+// }
+// console.log(getSubstringFromSecondIndex('salom'));
+
+
+// 13chi masala
+// function convertToUpperCase(str) {
+//     let res = str.toUpperCase()
+//     return res;
+// }
+// console.log(convertToUpperCase('salom'));
+
+
+// 14chi masala
+// function convertToLowerCase(str) {
+//     let res = str.toLowerCase()
+//     return res;
+// }
+// console.log(convertToLowerCase('SALOM'));
+
+
+// 15chi masala
+// function concatStrings(str1, str2) {
+//     let res = str1.concat(str2);
+//     return res;
+// }
+// console.log(concatStrings('salom'));
+
+
+// 16chi masala
+// function trimString(str) {
+//     let res = str.trim()
+//     return res
+// }
+// console.log(trimString('                             salom                '));
+
+
+// 17chi masala
+// function padStringStart(str) {
+//     let res = str.padStart(11, '*')
+//     return res
+// }
+// console.log(padStringStart('salom'));
+// 18 - masala
+// function padStringEnd(str) {
+//     let res = str.padEnd(11, '*')
+//     return res
+// }
+// console.log(padStringEnd('salom'));
+
+
+// 19chi masala
+// function trimEndString(str) {
+//     let res = str.trimEnd()
+//     return res
+// }
+// console.log(trimEndString('           salom                 '));
+
+
+// 20chi masala
+// function trimStartString(str) {
+//     let res = str.trimStart()
+//     return res
+// }
+// console.log(trimStartString('      salom              '));
+
+
+// 21chi masala
+// function padStringWithZerosStart(str) {
+//     let res = str.padStart(12, 0)
+//     return res
+// }
+// console.log(padStringWithZerosStart('salom'))
+
+
+// 22chi masala
+// function padStringWithZerosEnd(str) {
+//     let res = str.padEnd(12, 0)
+//     return res
+// }
+// console.log(padStringWithZerosEnd('salom'))
+
+
+// 23chi masala
+// function repeatString(str, N){
+// let res = str.repeat(N)
+// return res
+// }
+// console.log(repeatString('salom' ));
+
+
+// 24chi masala
+// function replaceAppleWithOrange(str){
+// let res = str.replace('Apple' , 'orange') ;
+// return res;
+// }
+// console.log(replaceAppleWithOrange('Apple Samsung'));
+
+
+// 25chi masala
+// function FirstSmallAToBigA(str){
+//     let res = str.replace('a' , 'A') ;
+//     return res;
+//     }
+//     console.log(FirstSmallAToBigA('Apple redmi'));
+
+// 26chi masala
+// function replaceAllCatsWithDogs(str){
+//     let res = str.replaceAll('cat' , 'dog') ;
+//     return res;
+//     }
+//     console.log(replaceAllCatsWithDogs('cat dog cat ',));
+
+
+// 27chi masala
+// function replaceAllSmallEToBigE(str){
+//     let res = str.replaceAll('e' , 'E') ;
+//     return res;
+//     }
+//     console.log(replaceAllSmallEToBigE('Samsung'));
+
+
+// 28chi masala
+// function splitBySpaces(str){
+// let res = str.split( )
+// return res
+// }
+// console.log(splitBySpaces('salom'));
+
+
+// 29chi masala
+// function splitByCommas(str){
+//     let res = str.split(" ")
+//     return res
+//     }
+//     console.log(splitByCommas('salom'));
+
+
+// 30chi masala
+// function splitByEachCharacter(str){
+//     let res = str.split("")
+//     return res
+//     }
+//     console.log(splitByEachCharacter('salom'));
+
+
+// 31chi masala
+// function splitByEachCharacter(str){
+//     let res = str.split("*");
+//     return res
+//     }
+//     console.log(splitByEachCharacter('salom'));
+
+// 32chi masala
+// (repeat, replace, split)**
+// function processString(str) {
+// let res = str.replaceAll('a','*');
+// let res1 = str.repeat(3); 
+// return res1, res
+// }
+// console.log(processString('salom '));
+
+
+// 45chi masala
+// function findFirstAppleIndex(str){
+//     return str.indexOf('@') 
+//     }
+//     console.log(findFirstAppleIndex('!#$%^&*@#$'));
+
+
+// 46chi masala
+// function searchBanana(str){
+// return str.search('salom');
+// }
+// console.log(searchBanana('salom '));
+
+
+// 47chi masala
+// function searchForDigit(str) {
+//     let index = str.search(/\d/);
+//     return index;
+// }
+// console.log(searchForDigit("salom"));
+
+
+// 50chi masala
+// function startsWithHello(str){
+// let res = str.startsWith('Hello')
+// return res
+// }
+// console.log(startsWithHello('Hello'));
